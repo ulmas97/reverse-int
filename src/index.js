094ac427ff5b577713f8d58d7f4fd7c55ab94ab1
ulmasbek <[rakhmatullaevulmas@gmail.com]>
2020-02-26 22:32:48 +0500

    feature:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) n = n * (-1);
+    var num = n.toString().split('');
+    var newNum = new Array(num.length);
+    num.forEach((element) => {
+        newNum.unshift(element);
+    });
+    n = Number(newNum.join(""));
+    return n;
+
 }
